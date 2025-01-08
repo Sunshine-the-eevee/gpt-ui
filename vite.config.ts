511acefd0d8b781a35a4c4e35a4db6a1c569c6bd
Sunshine the eevee <67440945+Sunshine-the-eevee@users.noreply.github.com>
2025-01-08 12:03:59 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { svelte } from '@sveltejs/vite-plugin-svelte';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte()],
-  base: '/',
+    base: '/gpt-ui/', // Replace <repository-name> with your GitHub repository’s name
   server: {
     host: '0.0.0.0', // Bind to all network interfaces, allowing external access
     port: process.env.PORT ? parseInt(process.env.PORT) : 5173, // Use Render's assigned PORT or default to 5173
