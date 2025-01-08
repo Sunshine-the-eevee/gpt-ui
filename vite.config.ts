7f2dfeb64e8bc96142c2c5e453b4ad9e2b41ee63
Sunshine the eevee <67440945+Sunshine-the-eevee@users.noreply.github.com>
2025-01-08 11:35:23 -0500

    Add files via upload

---
+++ vite.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from 'vite';
+import { svelte } from '@sveltejs/vite-plugin-svelte';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [svelte()],
+  base: '/',
+  server: {
+    host: '0.0.0.0', // Bind to all network interfaces, allowing external access
+    port: process.env.PORT ? parseInt(process.env.PORT) : 5173, // Use Render's assigned PORT or default to 5173
+  },
+});
